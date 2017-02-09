3b0e13e870507fa46ad60892c1e4a25c01239d36
Bobby <Bobby@DESKTOP-BVFTC53>
2017-02-09 14:11:02 -0500

    Add project files.

---
+++ Lab1/ComicBook.cs
@@ -0,0 +1,156 @@
+﻿//I, Bobby Filippopoulos, student number 000338236, certify that this material is my
+// original work. No other person's work has been used without due
+// acknowledgement and I have not made my work available to anyone else.
+// The programs purpose is to be able to read a text file that contains a list of comicbook, each comicbook contains details
+// the program will be to display the list of comicbook in a sorted format that is decided by the user
+// Date created: 1/22/2017
+// Date last modified: 2/3/2017
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Contains the parameters of the comicbook object, constructor and get/set methods
+    /// </summary>
+    class ComicBook
+    {
+        private string publisher;
+        private string title;
+        private int issue;
+        private string date;
+        private decimal bookValue;
+        private decimal marketValue;
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public ComicBook() { }
+        /// <summary>
+        /// this constructor sets the parameters of the ComicBook object
+        /// </summary>
+        /// <param name="publisher">string containing publisher name</param>
+        /// <param name="title">string containing title of the book</param>
+        /// <param name="issue">int containing the issue number of the book</param>
+        /// <param name="date">string containing the date written, formatted in main using DateTime allowing sorting</param>
+        /// <param name="bookValue">decimal containing the original value</param>
+        /// <param name="marketValue">decimal containing the market value of the book</param>
+        public ComicBook(string publisher, string title, int issue, string date, decimal bookValue, decimal marketValue)
+        {
+            this.publisher = publisher;
+            this.title = title;
+            this.issue = issue;
+            this.date = date;
+            this.bookValue = bookValue;
+            this.marketValue = marketValue;
+        }
+       /// <summary>
+       /// returns the objects publisher parameter
+       /// </summary>
+       /// <returns></returns>
+        public string GetPublisher()
+        {
+            return publisher;
+        }
+        /// <summary>
+        /// returns the objects title parameter
+        /// </summary>
+        /// <returns>string containing title</returns>
+        public string GetTitle()
+        {
+            return title;
+        }
+        /// <summary>
+        /// returns the objects issue parameter
+        /// </summary>
+        /// <returns>int containing issue</returns>
+        public int GetIssue()
+        {
+            return issue;
+        }
+        /// <summary>
+        /// returns the objects date parameter
+        /// </summary>
+        /// <returns>string containing date</returns>
+        public string GetDate()
+        {
+            return date;
+        }
+        /// <summary>
+        /// returns the objects bookvalue parameter
+        /// </summary>
+        /// <returns>decimal containing bookvalue</returns>
+        public decimal GetBookValue()
+        {
+            return bookValue;
+        }
+        /// <summary>
+        /// returns the objects marketvalue parameter
+        /// </summary>
+        /// <returns>decimal containing marketvalue</returns>
+        public decimal GetMarketValue()
+        {
+            return marketValue;
+        }
+        /// <summary>
+        /// changes the objects publisher value to the parameter value
+        /// </summary>
+        /// <param name="publisher"></param>
+        public void SetPublisher(string publisher)
+        {
+            this.publisher = publisher;
+        }
+        /// <summary>
+        /// changes the objects title value to the parameter value
+        /// </summary>
+        /// <param name="title">title value set by user</param>
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+        /// <summary>
+        /// changes the objects issue value to the parameter value
+        /// </summary>
+        /// <param name="issue">issue value set by user</param>
+        public void SetIssue(int issue)
+        {
+            this.issue = issue;
+        }
+        /// <summary>
+        /// changes the objects date value to the parameter value
+        /// </summary>
+        /// <param name="date">date value set by user</param>
+        public void SetDate(string date)
+        {
+            this.date = date;
+        }
+        /// <summary>
+        /// changes the objects bookvalue value to the parameter value
+        /// </summary>
+        /// <param name="bookValue">bookvalue value set by user</param>
+        public void SetBookValue(decimal bookValue)
+        {
+            this.bookValue = bookValue;
+        }
+        /// <summary>
+        /// changes the objects marketvalue value to the parameter value
+        /// </summary>
+        /// <param name="marketValue">marketvalue value set by user</param>
+        public void SetMarketValue(decimal marketValue)
+        {
+            this.marketValue = marketValue;
+        }
+
+       /// <summary>
+       /// returns a formatted string containing the parameters of the comicbook object to the console
+       /// </summary>
+        public void displayComic()
+        {
+           Console.WriteLine("{0,-10}  {1,-25}  {2,-5}  {3,-15}  {4,-10}  {5,-10}",  publisher,  title,  issue,  date,  bookValue,  marketValue);
+        }
+    }
+}
+    
---
+++ Lab1/Lab1.cs
@@ -0,0 +1,234 @@
+﻿//I, Bobby Filippopoulos, student number 000338236, certify that this material is my
+// original work. No other person's work has been used without due
+// acknowledgement and I have not made my work available to anyone else.
+// The programs purpose is to be able to read a text file that contains a list of comicbook, each comicbook contains details
+// the program will be to display the list of comicbook in a sorted format that is decided by the user
+// Date created: 1/22/2017
+// Last Date Modified: 2/3/2017
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    /// <summary>
+    ///  Function calls resides in main method, reading, sorting and output format are all contained within the lab1 class 
+    ///  reading, sorting and output makes use of the ComicBook class which contain the format for a comicbook object
+    /// </summary>
+    class Lab1
+    {
+        /// <summary>
+        /// Most functionality resides in this class, reading, sorting and output format are all contained within methods and called in the main class
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            ComicBook[] comicBookCollection;
+            int num;
+            Lab1.Read(out comicBookCollection,out num);
+            bool moreFunctions = true;
+
+            while (moreFunctions) //If the user enters '7' the flag will be set to false and the program will quit.
+            {
+                
+                string choice = menu(); //set sort selection to the return string from menu, request input and display menu
+                int choiceParsed;
+                if (Int32.TryParse(choice, out choiceParsed)) //convert the string to an integer, check for appropriate input
+                {
+                    switch (choiceParsed)
+                    {
+                        case 1:
+                            selectionSort(comicBookCollection, num, choiceParsed);
+                            break;
+                        case 2:
+                            selectionSort(comicBookCollection, num, choiceParsed);
+                            break;
+                        case 3:
+                            selectionSort(comicBookCollection, num, choiceParsed);
+                            break;
+                        case 4:
+                            selectionSort(comicBookCollection, num, choiceParsed);
+                            break;
+                        case 5:
+                            selectionSort(comicBookCollection, num, choiceParsed);
+                            break;
+                        case 6:
+                            selectionSort(comicBookCollection, num, choiceParsed);
+                            break;
+                        case 7:
+                            moreFunctions = false;
+                            break;
+                        default:
+                            Console.WriteLine("");
+                            Console.WriteLine("Invalid choice, please try again.");
+                            Console.WriteLine("");
+                            break;
+                    }
+                    if (choiceParsed >= 1 && choiceParsed <= 6)
+                    {
+                        Console.Clear();
+                        DisplayComicTable(comicBookCollection, num);
+                        
+                    }
+                }
+                
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid choice, please try again.");
+                    Console.WriteLine("");
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// This method prompts the user to choose which parameter to sort by
+        /// </summary>
+        /// <returns>returns the users choice bound within 1-7</returns>
+        private static string menu()
+        {
+            Console.WriteLine("1. Sort by Publisher Name");
+            Console.WriteLine("2. Sort by Comic Title");
+            Console.WriteLine("3. Sort by Issue Number");
+            Console.WriteLine("4. Sort by Cover Date");
+            Console.WriteLine("5. Sort by Cover Value");
+            Console.WriteLine("6. Sort by Market Value");
+            Console.WriteLine();
+            Console.WriteLine("7. Exit");
+            Console.Write("Enter your choice: ");
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// This sorting method is a selection sort. With an average case of O(n2) it is suitable for this data set.
+        /// This sort will move through the array setting the minimum value to most left index. After each round
+        /// The pointer will move to the right by one index and move through the array finding the minimum value.
+        /// This will repeat until the end of the array.
+        /// </summary>
+        /// <param name="comicBookCollection">object array to sort</param>
+        /// <param name="size">size of the array</param>
+        /// <param name="choice">gathered from the call to menu, decides which parameter to sort by</param>
+        private static void selectionSort(ComicBook[] comicBookCollection, int size, int choice)
+        {
+            int startScan;
+            int index;
+            int minIndex;
+            ComicBook minValue; //create object that will contain the temporary information awaiting switches
+
+            for (startScan = 0; startScan < size; startScan++)
+            {
+                minIndex = startScan;
+                minValue = comicBookCollection[startScan];
+
+                for (index = startScan + 1; index < size; index++)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            //implement case structure to dictate the pull from object e.g getissue
+                            if (minValue.GetPublisher().CompareTo(comicBookCollection[index].GetPublisher()) > 0)
+                            {
+                                minValue = comicBookCollection[index];
+                                minIndex = index;
+                            }
+                            break;
+                        case 2:
+                            if (minValue.GetTitle().CompareTo(comicBookCollection[index].GetTitle()) > 0)
+                            {
+                                minValue = comicBookCollection[index];
+                                minIndex = index;
+                            }
+                            break;
+                        case 3:
+                            if (comicBookCollection[index].GetIssue() < minValue.GetIssue())
+                            {
+                                minValue = comicBookCollection[index];
+                                minIndex = index;
+                            }
+                            break;
+                        case 4:
+                            
+                            if (DateTime.Parse(minValue.GetDate()) > (DateTime.Parse(comicBookCollection[index].GetDate())))
+                            {
+                                minValue = comicBookCollection[index];
+                                minIndex = index;
+                            }
+                            break;
+                        case 5:
+                            if (comicBookCollection[index].GetBookValue() > minValue.GetBookValue())
+                            {
+                                minValue = comicBookCollection[index];
+                                minIndex = index;
+                            }
+                            break;
+                        case 6:
+                            if (comicBookCollection[index].GetMarketValue() > minValue.GetMarketValue())
+                            {
+                                minValue = comicBookCollection[index];
+                                minIndex = index;
+                            }
+                            break;
+                    }
+                }
+                comicBookCollection[minIndex] = comicBookCollection[startScan];
+                comicBookCollection[startScan] = minValue;
+            }
+            
+        }
+
+        /// <summary>
+        /// this read method goes through the text file line by line, over writing an array with each line
+        /// after the array is set, a new comic will be created with the contents of the array
+        /// </summary>
+        /// <param name="comicBookCollection">sets the objects data</param>
+        /// <param name="num">sets the size of the array, needed to display and sort</param>
+        private static void Read(out ComicBook[] comicBookCollection, out int num)
+        {
+            num = 0;
+            comicBookCollection = new ComicBook[100];
+            try
+            {
+                string filename = "comics.txt";
+                string fullFileName = Path.Combine(Directory.GetCurrentDirectory(), filename); //this creates the absolute path of the file
+                FileStream file = new FileStream(fullFileName, FileMode.Open);
+                StreamReader data = new StreamReader(file);
+
+                string comicLine;
+                while ((comicLine = data.ReadLine()) != null) //read until the end of file
+                {
+                    string[] comicLineSplit = comicLine.Split(',');
+                    comicBookCollection[num] = new ComicBook(comicLineSplit[0].Trim(), comicLineSplit[1].Trim(), Convert.ToInt32(comicLineSplit[2]), comicLineSplit[3].Trim(), Convert.ToDecimal(comicLineSplit[4]), Convert.ToDecimal(comicLineSplit[5]));
+                    
+                    num++;
+                }
+                data.Close();
+                file.Close();
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+        }
+
+        /// <summary>
+        /// This method outputs the content of the comicBookCollect
+        /// </summary>
+        /// <param name="comicBookCollection">the comicbook array to display</param>
+        /// <param name="num">the amount of comics that will be displayed</param>
+        private static void DisplayComicTable(ComicBook[] comicBookCollection,int num)
+        {
+            Console.WriteLine("{0,-10}  {1,-25}  {2,-5}  {3,-15}  {4,-10}  {5,-10}", "Publisher", "Title", "Issue", "Date", "Book Value", "Market Value");
+            Console.WriteLine("{0,-10}  {1,-25}  {2,-5}  {3,-15}  {4,-10}  {5,-10}", "==========", "======================", "=====", "========", "==========", "============");
+            for (int i = 0; i < num; i++)
+            {
+                comicBookCollection[i].displayComic();
+            }
+            Console.WriteLine("");
+        }
+    }
+}
